d370ba820281b224bc32a108ac1a7f04e786cf8b
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-20 21:03:27 +0500

    djk cs

--- api/lib/api.dart
+++ api/lib/api.dart
@@ -1,1 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class Api extends StatefulWidget {
+  const Api({super.key});
+
+  @override
+  State<Api> createState() => _ApiState();
+}
+
+class _ApiState extends State<Api> {
+  @override
+
+  Future<void> fetchData ()
+  async{
+
+    final url=Uri.parse('https://reqres.in/api/unknown');
+    final response= await http.get(url);
+
+  }
+
+
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

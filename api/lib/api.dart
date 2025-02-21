ac877dece1466bc5034d0a7aa8c3b073f7049e2a
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-21 16:06:53 +0500

    update

--- api/lib/api.dart
+++ api/lib/api.dart
@@ -1,9 +1,77 @@
-import 'dart:convert';
+// import 'dart:convert';
+//
+// import 'package:api/src/user_model/user_model.dart';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+//
+//
+// class Api extends StatefulWidget {
+//   const Api({super.key});
+//
+//   @override
+//   State<Api> createState() => _ApiState();
+// }
+//
+// class _ApiState extends State<Api> {
+//   @override
+//
+//   List <UserModel> userList=[];
+//
+//   Future<void> fetchData ()
+//   async{
+//
+//     final url=Uri.parse('https://reqres.in/api/unknown');
+//     final response= await http.get(url);
+//     if(response.statusCode==200)
+//       {
+//         final data=json.decode(response.body);
+//        setState(() {
+//          userList=(data['data' as List]).
+//          map((user)=>UserModel.fromMap(user)).toList();
+//        });
+//       }
+//     else
+//       {
+//         print("ERRRRRROROOROROROORR");
+//
+//       }
+//
+//     @override
+//     void initState() {
+//       // TODO: implement initState
+//       super.initState();
+//       fetchData();
+//     }
+//   }
+//
+//
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Api'),
+//         centerTitle:true,
+//       ),
+//       body: ListView.builder(
+//         itemCount: userList.length,
+//         itemBuilder: (context,index){
+//           return Column(children: [
+//             Text(userList[index].id.toString()),
+//             Text(userList[index].name.toString()),
+//             Text(userList[index].year.toString()),
+//             Text(userList[index].color.toString()),
+//             Text(userList[index].pantone_value.toString()),
+//           ],);
+//         },
+//       ),
+//     );
+//   }
+// }
 
-import 'package:api/src/user_model/user_model.dart';
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:api/src/user_model/user_model.dart';
 
 class Api extends StatefulWidget {
   const Api({super.key});
@@ -13,54 +81,53 @@ class Api extends StatefulWidget {
 }
 
 class _ApiState extends State<Api> {
+  List<UserModel> userList = [];
+
   @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
 
-  List <UserModel> userList=[];
+  Future<void> fetchData() async {
+    final url = Uri.parse('https://reqres.in/api/unknown');
+    final response = await http.get(url);
 
-  Future<void> fetchData ()
-  async{
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
 
-    final url=Uri.parse('https://reqres.in/api/unknown');
-    final response= await http.get(url);
-    if(response.statusCode==200)
-      {
-        final data=json.decode(response.body);
-       setState(() {
-         userList=(data['data' as List]).
-         map((user)=>UserModel.fromMap(user)).toList();
-       });
-      }
-    else
-      {
-        print("ERRRRRROROOROROROORR");
-
-      }
-
-    @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      fetchData();
+      setState(() {
+        List<dynamic> dataList = data['data'];
+        userList = dataList.map((user) => UserModel.fromMap(user)).toList();
+      });
+    } else {
+      print("Error fetching data");
     }
   }
 
-
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Api'),
-        centerTitle:true,
+        title: Text('API Data'),
+        centerTitle: true,
       ),
-      body: ListView.builder(
+      body: userList.isEmpty
+          ? Center(child: CircularProgressIndicator()) // Show loader until data is fetched
+          : ListView.builder(
         itemCount: userList.length,
-        itemBuilder: (context,index){
-          return Column(children: [
-            Text(userList[index].id.toString()),
-            Text(userList[index].name.toString()),
-            Text(userList[index].year.toString()),
-            Text(userList[index].color.toString()),
-            Text(userList[index].pantone_value.toString()),
-          ],);
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(8),
+            child: ListTile(
+              title: Text(userList[index].name ?? 'No Name'),
+              subtitle: Text("Year: ${userList[index].year}"),
+              leading: CircleAvatar(
+                backgroundColor: userList[index].color ?? Colors.grey,
+              ),
+              trailing: Text(userList[index].pantone_value ?? ''),
+            ),
+          );
         },
       ),
     );

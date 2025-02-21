afad513e05008f372d5d1298a9e8e0b6e9179730
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-21 23:09:23 +0500

    ;ksd jv

--- api/lib/src/model/model.dart
+++ api/lib/src/model/model.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-class supportModel
+class SupportModel
 {
   String? url;
   String? text;
+
+  SupportModel({required this.url,required this.text});
+
+  static SupportModel fromMap (Map<String,dynamic>map){
+
+  return SupportModel(
+    url: map['url'],
+    text:map['text']
+);
+
+}
+
 }

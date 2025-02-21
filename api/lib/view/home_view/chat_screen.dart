9a506b643ff2c14a7efdaae61158b844d3ab398f
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-21 22:47:01 +0500

    Update chat_screen.dart

--- api/lib/view/home_view/chat_screen.dart
+++ api/lib/view/home_view/chat_screen.dart
@@ -5,6 +5,10 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+    );
   }
 }

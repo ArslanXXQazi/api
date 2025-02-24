2b43b3d757a9b540ed811a6170ac4110bf7ef2e0
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-24 21:40:02 +0500

    Update custom_text_form_field.dart

--- api/lib/src/controller/components/custom_text_form_field.dart
+++ api/lib/src/controller/components/custom_text_form_field.dart
@@ -5,6 +5,8 @@ class CustomTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextFormField(
+      
+    );
   }
 }

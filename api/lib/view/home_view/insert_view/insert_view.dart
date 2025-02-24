f47009a87f251addd0d30c9609c86fdeea3bc814
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-24 21:32:50 +0500

    Update insert_view.dart

--- api/lib/view/home_view/insert_view/insert_view.dart
+++ api/lib/view/home_view/insert_view/insert_view.dart
@@ -3,5 +3,20 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+class InsertView extends StatefulWidget {
+  const InsertView({super.key});
+
+  @override
+  State<InsertView> createState() => _InsertViewState();
+}
+
+class _InsertViewState extends State<InsertView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
 
 

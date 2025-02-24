8bd712ddcdec58a02aec1aa6a0ec712fbab04304
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-24 21:31:13 +0500

    Update insert_view.dart

--- api/lib/view/home_view/insert_view/insert_view.dart
+++ api/lib/view/home_view/insert_view/insert_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:api/view/home_view/chat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:api/src/user_model/user_model.dart';
+
+

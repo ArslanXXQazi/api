3eb397a507cd2e3ffa7d6b14b98f28228047ddac
Arslan Qazi <arslanqazi1999@gmail.com>
2025-02-24 21:31:32 +0500

    Update insert_view.dart

--- api/lib/view/home_view/insert_view/insert_view.dart
+++ api/lib/view/home_view/insert_view/insert_view.dart
@@ -3,6 +3,6 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:api/src/user_model/user_model.dart';
+
 
 

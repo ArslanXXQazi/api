0efd5210de20ed1e09d71aa91ed343aeaf1424e7
Arslan Qazi <arslanqazi1999@gmail.com>
2025-10-13 23:57:01 +0500

    o

--- api/lib/src/get/get-controller.dart
+++ api/lib/src/get/get-controller.dart
@@ -1,6 +1,37 @@
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class GetController extends GetxController
 {
-  
+
+
+ var id=0;
+ var firstName = "";
+ var lastName = "";
+ var gender = "";
+ var age = "";
+
+ void fetchData  () async
+ {
+   try{
+     Dio dio =Dio();
+     final response = await dio.get('https://dummyjson.com/users/1');
+     final data = response.data;
+     firstName=data['firstName'];
+     lastName=data['lastName'];
+     gender=data['female'];
+     age=data['age'];
+     id=data['id'];
+   }
+   catch (e){
+
+     print(e.toString());
+
+   }
+
+
+ }
+
+
+
 }

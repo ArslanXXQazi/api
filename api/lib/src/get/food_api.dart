87364ea754c64eaf8a31d1531c8c2b0124afa519
Arslan Qazi <arslanqazi1999@gmail.com>
2025-10-18 12:10:03 +0500

    d

---
+++ api/lib/src/get/food_api.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FoodApi extends StatefulWidget {
+  const FoodApi({super.key});
+
+  @override
+  State<FoodApi> createState() => _FoodApiState();
+}
+
+class _FoodApiState extends State<FoodApi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.white,
+
+
+    );
+  }
+}

b9751a549ace7e603c0d9278c706f1e3313aecb2
Arslan Qazi <arslanqazi1999@gmail.com>
2025-10-19 11:05:17 +0500

    d

---
+++ api/lib/src/get/get_storage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class InsertData extends StatelessWidget {
+  const InsertData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

f5e52c2602dfd7767dd89adf294f789fabf74382
Arslan Qazi <arslanqazi1999@gmail.com>
2025-10-20 12:41:05 +0500

    s

--- api/lib/src/post/post_insert_data.dart
+++ api/lib/src/post/post_insert_data.dart
@@ -3,6 +3,8 @@
 import 'package:api/src/controller/components/black-text.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class PostInsertData extends StatefulWidget {
   const PostInsertData({super.key});
@@ -20,23 +22,64 @@ class _PostInsertDataState extends State<PostInsertData> {
   void insertData () async
   {
 
-    String postUrl = 'https://api.restful-api.dev/objects';
-    final Map<String,dynamic> requestBody = {
-      "name" : nameController.text,
-      "year" : int.parse(yearController.text),
-      "price": double.parse(priceController.text),
-      "CPU model" : cpuModelController.text,
-      "Hard disk size" : hardDiskSizeController.text
-    };
-    Dio dio = Dio();
-  final response = await  dio.post(
-        postUrl,
-        data: requestBody
-    );
+    try{
 
-  if (response.statusCode == 200 || response.statusCode== 201)
-    {
-      
+      isLoading=true;
+      setState(() {
+
+      });
+      String postUrl = 'https://api.restful-api.dev/objects';
+      final Map<String,dynamic> requestBody = {
+        "name" : nameController.text,
+        "year" : int.parse(yearController.text),
+        "price": double.parse(priceController.text),
+        "CPU model" : cpuModelController.text,
+        "Hard disk size" : hardDiskSizeController.text
+      };
+      Dio dio = Dio();
+      final response = await  dio.post(
+          postUrl,
+          data: requestBody
+      );
+
+      if (response.statusCode == 200 || response.statusCode== 201)
+      {
+        Get.snackbar(
+          "Success",
+          "Data Inserted Successfully",
+          backgroundColor: Colors.green
+
+        );
+        isLoading=false;
+        setState(() {});
+
+      }
+      else
+        {
+          isLoading=false;
+          setState(() {});
+          Get.snackbar(
+            "ERROR",
+            "Data not inserted due to error",
+            backgroundColor: Colors.red
+
+          );
+        }
+
+
+    }
+    catch(e){
+      isLoading=false;
+      setState(() {
+
+      });
+      error = e.toString();
+      print('Error: $error'); // Debugging
+      Get.snackbar(
+        "ERROR",
+        "Failed to insert data: $error",
+        backgroundColor: Colors.red,
+      );
     }
 
 
@@ -57,7 +100,17 @@ class _PostInsertDataState extends State<PostInsertData> {
         title: Blacktext(text: "POST API",fontSize: 34,fontWeight: FontWeight.w700),
         centerTitle: true,
       ),
-      body: Padding(
+      body: error.isNotEmpty ? Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Blacktext(text: error,fontSize: 16,color: Colors.red,),
+          SizedBox(height: 20),
+          ElevatedButton(onPressed: (){
+            insertData();
+            }, child: Blacktext(text: "Retry"))
+        ],
+      )):
+      Padding(
         padding:  EdgeInsets.all(30),
         child: SingleChildScrollView(
           child: Column(children: [
@@ -71,11 +124,13 @@ class _PostInsertDataState extends State<PostInsertData> {
             SizedBox(height: 20),
             TextFormField(controller: hardDiskSizeController,decoration: InputDecoration(hintText: "Enter Hard Size"),),
             SizedBox(height: 30),
-            ElevatedButton(onPressed: (){}, child: Blacktext(text: "SAVE",fontSize: 16,)),
+            isLoading ? Center(child: CircularProgressIndicator()):
+            ElevatedButton(onPressed: (){
+              insertData();
+            }, child: Blacktext(text: "SAVE",fontSize: 16,)),
           ],),
         ),
-      ),
-
+      )
 
     );
   }

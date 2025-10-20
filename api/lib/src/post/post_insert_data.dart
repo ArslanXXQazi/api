54688107f4aef52d6757fdd2c70937cc378250d9
Arslan Qazi <arslanqazi1999@gmail.com>
2025-10-20 11:43:46 +0500

    Update post_insert_data.dart

--- api/lib/src/post/post_insert_data.dart
+++ api/lib/src/post/post_insert_data.dart
@@ -31,9 +31,17 @@ class _PostInsertDataState extends State<PostInsertData> {
         child: Column(children: [
 
 
-          
-
-
+          TextFormField(controller: nameController,decoration: InputDecoration(hintText: "Enter Name"),),
+          SizedBox(height: 20),
+          TextFormField(controller: yearController,decoration: InputDecoration(hintText: "Enter Year"),),
+          SizedBox(height: 20),
+          TextFormField(controller: priceController,decoration: InputDecoration(hintText: "Enter Price"),),
+          SizedBox(height: 20),
+          TextFormField(controller: cpuModelController,decoration: InputDecoration(hintText: "Enter CPU Model"),),
+          SizedBox(height: 20),
+          TextFormField(controller: hardDiskSizeController,decoration: InputDecoration(hintText: "Enter Hard Size"),),
+          SizedBox(height: 30),
+          ElevatedButton(onPressed: (){}, child: Blacktext(text: "SAVE",fontSize: 16,)),
         ],),
       ),
 

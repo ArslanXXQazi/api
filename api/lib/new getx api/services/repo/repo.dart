6ef8d4530c460efcef44efa65d0449a70d5758dc
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-17 10:56:03 +0500

    s

--- api/lib/new getx api/services/repo/repo.dart
+++ api/lib/new getx api/services/repo/repo.dart
@@ -1,15 +1,25 @@
 
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
 class GetRepo
 {
 
-  Future <Map<String,dynamic>> getData async ()
+  Future <Map<String,dynamic>> getData () async
   {
-
     var dio = Dio();
-    
+    String getUrl ="https://dummyjson.com/users/1";
+    final response = await dio.get(getUrl);
+    final data = response.data;
 
+    if (response.statusCode ==200)
+      {
+        return data;
+      }
+    else {
+      throw Exception(response.statusMessage);
+    }
   }
 
 }

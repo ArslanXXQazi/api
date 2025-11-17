0e98afd43a6a5d7d836a4b333e4ed78181975b52
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-17 13:13:32 +0500

    Update user_details_screen.dart

--- api/lib/new getx api/screens/user_details_screen.dart
+++ api/lib/new getx api/screens/user_details_screen.dart
@@ -4,20 +4,30 @@ import 'package:api/src/controller/components/black-text.dart';
 import 'package:flutter/material.dart';
 
 class UserDetailsScreen extends StatelessWidget {
+
+
+
   const UserDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Blacktext(text: "User Detail"),),
-      body: Column(children: [
+      appBar: AppBar(title: Blacktext(text: "User Detail",fontSize: 30,fontWeight: FontWeight.w700,),centerTitle: true,),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
 
-        CircleAvatar(
-          radius: 30,
-        )
+          Center(
+            child: CircleAvatar(
+              radius: 50,
+            ),
+          ),
 
 
-      ],),
+        ],),
+      ),
     );
   }
 }

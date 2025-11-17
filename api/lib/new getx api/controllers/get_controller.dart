837cc980490afa93f498ad8391cc7989a842baab
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-17 11:02:24 +0500

    s

--- api/lib/new getx api/controllers/get_controller.dart
+++ api/lib/new getx api/controllers/get_controller.dart
@@ -1,9 +1,31 @@
+import 'package:api/new%20getx%20api/services/repo/repo.dart';
 import 'package:get/get.dart';
 
 class GetxGetController extends GetxController
 {
 
   RxMap<String , dynamic> userData = RxMap<String,double>();
+
+  final GetRepo repo = GetRepo();
+
+  @override
+  void onInit() {
+    super.onInit();
+
+  }
+
+  void fetchUserData () async
+  {
+
+    try {
+      
+    }
+    catch(e){
+
+    }
+
+
+  }
 
 
 }

741af3d67e227bd96e9304650358ce750cbc0a0a
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-24 15:19:17 +0500

    s

--- api/lib/new getx api/services/repo/register_repo.dart
+++ api/lib/new getx api/services/repo/register_repo.dart
@@ -1,9 +1,50 @@
 
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
 class RegisterRepo
 {
 
 
-  
+  Dio dio = Dio();
+  Future<void> register(String name , String lastName, String email, String password) async
+  {
+
+    String registerUrl = "https://etalk.mtai.live/api/user/register";
+    final data = json.encode({
+
+      "name":name,
+      "email":email,
+      "password":password,
+      "confirm_password":password,
+
+    });
+
+
+
+    try{
+
+      final response = await dio.post(
+          registerUrl,
+          data: data
+      );
+
+      if(response.statusCode==200 || response.statusCode==201)
+        {
+
+        }
+
+
+    }
+    catch(e){
+
+    }
+
+
+  }
+
+
 
 
 

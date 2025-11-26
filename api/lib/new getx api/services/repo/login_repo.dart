8366c0dc7e293817d053b3953455afc42e2d3e69
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-26 12:44:20 +0500

    Update login_repo.dart

--- api/lib/new getx api/services/repo/login_repo.dart
+++ api/lib/new getx api/services/repo/login_repo.dart
@@ -5,10 +5,22 @@ class LoginRepo{
 
   Dio dio = Dio();
 
-  Future<dynamic> loginRepo () async {
+  Future<dynamic> loginRepo (String email,String password) async {
+
+    try{
+      String loginUrl = "https://etalk.mtai.live/api/user/login";
+      final Data = {
+        "email":email,
+        "password":password,
+        "fcm_token":"abcd"
+      };
+    }
+    catch(e){
+
+    }
+
+
 
   }
-
-
 
 }

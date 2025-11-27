cdbea11a4c439b46a384c5ad75875d9d19935603
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-27 18:12:24 +0500

    s

--- api/lib/new getx api/screens/bottom_nav_screen.dart
+++ api/lib/new getx api/screens/bottom_nav_screen.dart
@@ -1,4 +1,5 @@
 import 'package:api/new%20getx%20api/screens/home_screen.dart';
+import 'package:api/new%20getx%20api/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,62 +11,53 @@ class BottomNavScreen extends StatefulWidget {
 }
 
 class _BottomNavScreenState extends State<BottomNavScreen> {
-
   List<Widget> screens = [
     HomeScreen(),
     Center(child: Text("Search Screen", style: TextStyle(fontSize: 25))),
     Center(child: Text("Notifications Screen", style: TextStyle(fontSize: 25))),
-    Center(child: Text("Profile Screen", style: TextStyle(fontSize: 25))),
+    ProfileScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
-
     BottomNavController controller = Get.put(BottomNavController());
 
     return Scaffold(
-      body: Obx(()=> screens[controller.currentIndex.value]),
-      bottomNavigationBar: Obx((){
-
-       return BottomNavigationBar(
-          currentIndex: controller.currentIndex.value,
-          onTap: (index)=>controller.changeIndex(index),
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.grey,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: "Search",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications),
-              label: "Notify",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile",
-            ),
-          ],
-        );
-
-
-      })
-    );
+        body: Obx(() => screens[controller.currentIndex.value]),
+        bottomNavigationBar: Obx(() {
+          return BottomNavigationBar(
+            currentIndex: controller.currentIndex.value,
+            onTap: (index) => controller.changeIndex(index),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.notifications),
+                label: "Notify",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "Profile",
+              ),
+            ],
+          );
+        }));
   }
 }
 
-class BottomNavController extends GetxController
-{
-  var currentIndex=0.obs;
+class BottomNavController extends GetxController {
+  var currentIndex = 0.obs;
 
-  void changeIndex( int index)
-  {
-    currentIndex.value=index;
+  void changeIndex(int index) {
+    currentIndex.value = index;
   }
-
 }

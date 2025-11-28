24cb28d5129108e6cc53b466c7ed1db6c06f7cd2
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-28 15:10:58 +0500

    s

--- api/lib/new getx api/services/repo/profile_repo.dart
+++ api/lib/new getx api/services/repo/profile_repo.dart
@@ -6,10 +6,34 @@ class ProfileRepo {
 
   Future<dynamic> profileRepo (String userId , String token) async {
 
+    try
+    {
 
-    String profileUrl = "https://etalk.mtai.live/api/user/$userId/profile";
+      String profileUrl = "https://etalk.mtai.live/api/user/$userId/profile";
+      final response = await dio.get(
+          profileUrl,
+          options: Options(
+            headers: {"Authorization": "Bearer $token"}
+          ));
 
-    
+      if (response.statusCode == 200 || response.statusCode==201)
+        {
+          print("===>> Data fetch Successfully");
+          return response.data;
+        }
+      else
+        {
+          print("===>> Data fetch Faild");
+          return response.data;
+        }
+
+
+    }
+    catch(e)
+    {
+      print("===>> Data fetch Faild ${e.toString()}");
+      return null;
+    }
 
   }
 

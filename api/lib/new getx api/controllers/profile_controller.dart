714244bd2a29ae476bdce72b153cda02a37ee433
Arslan Qazi <arslanqazi1999@gmail.com>
2025-11-28 15:18:22 +0500

    Update profile_controller.dart

--- api/lib/new getx api/controllers/profile_controller.dart
+++ api/lib/new getx api/controllers/profile_controller.dart
@@ -1,9 +1,36 @@
+import 'package:api/new%20getx%20api/services/repo/profile_repo.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController
 {
+  var isLoading = false.obs;
+  var profileData = {}.obs;
 
 
 
-  
+  Future <void> fetchProfileData (String userId, String token) async
+  {
+
+    ProfileRepo profileRepo = ProfileRepo();
+
+    try
+    {
+      isLoading.value=true;
+
+      final response = await profileRepo.profileRepo(userId, token);
+
+      
+
+
+
+    }
+    catch(e)
+    {
+
+    }
+
+
+  }
+
+
 }
